f8a552f0965324a4bc53f0a76adb7d5512331caa
candicehou <yujieh2@illinois.edu>
2018-12-04 21:39:25 -0600

    list view

--- app/src/main/java/com/example/rainbow/classchat/User.java
+++ app/src/main/java/com/example/rainbow/classchat/User.java
@@ -1,9 +1,22 @@
 package com.example.rainbow.classchat;
 
+import android.widget.ListView;
+
+import java.util.ArrayList;
 import java.util.List;
+//import java.util.Map;
+//import java.util.Set;
 
 public class User {
     private String username;
     private String password;
-    public List<Class> myClasses;
+    public static List<String> myClasses = new ArrayList<String>();
+
+
+
+
+    //public Map<String, Class> myClasses;
+
+    //Set<String> keys = myClasses.keySet();
+    //public String[] nameOfMyClasses = keys.toArray(new String[keys.size()]);//store the name of myClasses
 }
--- app/src/main/java/com/example/rainbow/classchat/UserActivity.java
+++ app/src/main/java/com/example/rainbow/classchat/UserActivity.java
@@ -4,14 +4,33 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserActivity extends AppCompatActivity {
 
+    ListView listView;
+    
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
+
+        listView = (ListView) findViewById(R.id.window_List);
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.activity_list_item, User.myClasses.toArray());
+        listView.setAdapter(arrayAdapter);
+
+
     }
+
     public void joinClass(View view){
         startActivity(new Intent(UserActivity.this, JoinActivity.class));
     }
+
+
+
+
+
 }
